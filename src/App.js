179953de155e47d81f06ebaffa2ba08a8900e55e
David Moates <david.m.moates@gmail.com>
2019-09-25 00:34:44 -0500

    added axios and yarn.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+
+  axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY') //had to {sudo yarn add axios} to get this to work
+    .then( response => {
+      console.log(response);
+    })
+    .catch(error => {
+      console.log('Error:', error);
+    });
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+
     </div>
   );
 }
