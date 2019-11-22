5bafef125216b37063aa5c0d78cc35156c146ef2
David Moates <david.m.moates@gmail.com>
2019-11-21 20:17:45 -0600

    axios called

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import axios from 'axios';
+
 import "./App.css";
 
 function App() {
+
+
+  axios
+    .get("https://api.nasa.gov/planetary/apod?api_key=iMZDV67iJVxqg1nh1BTK7GFS1S9GeeS9txAMgbxH") //had to {sudo yarn add axios} to get this to work
+    .then(response => {
+      console.log(response);
+    })
+    .catch(error => {
+      console.log("Error:", error);
+    });
+
+
+
   return (
     <div className="App">
       <p>
